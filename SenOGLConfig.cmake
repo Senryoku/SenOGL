a5371cc82f28247ea41eb9c8e3c113816e5b3d3b
Senryoku <maretverdant@gmail.com>
2015-01-09 00:53:52 +0100

    Improved CMake files

--- SenOGLConfig.cmake
+++ SenOGLConfig.cmake
@@ -1,4 +1,4 @@
 get_filename_component(SenOGL_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-set(SenOGL_INCLUDE_DIRS "${SenOGL_CMAKE_DIR}/external;${SenOGL_CMAKE_DIR}/include/textures;${SenOGL_CMAKE_DIR}/include/shaders;${SenOGL_CMAKE_DIR}/include")
+set(SenOGL_INCLUDE_DIRS "${SenOGL_CMAKE_DIR}/external" "${SenOGL_CMAKE_DIR}/include/textures" "${SenOGL_CMAKE_DIR}/include/shaders" "${SenOGL_CMAKE_DIR}/include")
 set(SenOGL_LIBRARY_DIRS "${SenOGL_CMAKE_DIR}/lib")
 set(SenOGL_LIBRARY SenOGL)
